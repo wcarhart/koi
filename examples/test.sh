7f1e12e9cef1410ddaf63f9f72ebf89767bbc743
Will Carhart <wcarhart@sandiego.edu>
2020-03-29 14:18:10 -0700

    First prototype of improved word wrapping

---
+++ examples/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+source ../koi
+
+function testfunc {
+	__addarg "-h" "--help" "help" "optional" "" "Help text"
+	__addarg "" "mycoolarg" "positionalarray" "required" "" "A really, really, really, really, really long help text that goes on and on and on for a really long time to make a point"
+	__addarg "-f" "--flag" "flag" "optional" "" "Another argument, this time a flag, with a super, super, super long description that's way longer than it should be"
+	__addarg "-g" "--glag" "flag" "optional" "" "Another argument, this time a flag, with a super, super, super long description that's way longer than it should be"
+	__parseargs "$@"
+
+	for a in "${mycoolarg[@]}" ; do
+		echo "found arg: '$a'"
+	done
+}
+
+__koirun "$@"
