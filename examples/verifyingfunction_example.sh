72807096d795b3a20fc735ba66f010654465a648
Will Carhart <wcarhart@sandiego.edu>
2020-04-05 14:51:45 -0700

    Working on verifyingfunction

---
+++ examples/verifyingfunction_example.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+source ../koi
+koiname=verifyingfunction_example.sh
+koidescription="Examples of verifying functions"
+
+function run {
+	__addarg "-h" "--help" "help" "optional" "" "Help text"
+	__addarg "" "--file" "positionalvalue" "required" "" "A file argument" "__verifyfile"
+	__parseargs "$@"
+	for f in "${file[@]}" ; do
+		echo "File: $f"
+	done
+}
+
+function __verifyfile {
+	if [[ ! -f "$1" ]] ; then
+		__errortext "$koiname: err: no such file '$1'"
+		return 1
+	fi
+}
+
+__koirun "$@"
