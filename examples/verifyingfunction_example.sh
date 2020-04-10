ce818e56d50336bfd3d49218779b83f165577f65
Will Carhart <wcarhart@sandiego.edu>
2020-04-10 10:08:04 -0700

    Finished implementation of verifyingfunction

--- examples/verifyingfunction_example.sh
+++ examples/verifyingfunction_example.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-source ../koi
+source koi
 koiname=verifyingfunction_example.sh
 koidescription="Examples of verifying functions"
 
 function run {
 	__addarg "-h" "--help" "help" "optional" "" "Help text"
-	__addarg "" "--file" "positionalvalue" "required" "" "A file argument" "__verifyfile"
+	__addarg "" "file" "positionalarray" "required" "" "A file argument" "__verifyfile"
 	__parseargs "$@"
+
 	for f in "${file[@]}" ; do
 		echo "File: $f"
 	done
