8500c23946702928e7c8d2f2e4ec229ba4a0a006
Will Carhart <wcarhart@sandiego.edu>
2020-04-11 19:56:46 -0700

    Prototype test framework

---
+++ tests/test_shortoptions/test_valid_shortoption.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+koirequirehelpactions=0
+
+# ========= TESTS ========= #
+function test_no_shortoption_with_longoption {
+	__addarg "" "--long" "storevalue" "optional" "" "help text"
+	__parseargs "$@"
+	echo "$long"
+}
+
+function test_valid_shortoption_singular {
+	__addarg "-a" "--aaa" "storevalue" "optional" "" "help text"
+	__parseargs "$@"
+	echo "$aaa"
+}
+
+function test_valid_shortoption_multiple {
+	__addarg "-a" "--aaa" "storevalue" "optional" "" "help text"
+	__addarg "-b" "--bbb" "storevalue" "optional" "" "help text"
+	__parseargs "$@"
+	echo "$aaa $bbb"
+}
+
+# ========= ASSERTIONS ========= #
+function koitest_run {
+	runtest test_no_shortoption_with_longoption long "--long" "long"
+	runtest test_valid_shortoption_singular arg "-a" "arg"
+	runtest test_valid_shortoption_multiple "arg barg" "-a" "arg" "-b" "barg"
+}
