41656616a19c92439468b712ec351ca80b9b5c5c
Will Carhart <carhartwill@gmail.com>
2020-05-12 20:29:09 -0700

    Added test for joint arg bug

--- tests/test_actions/test_action_flag/test_action_flag_valid.sh
+++ tests/test_actions/test_action_flag/test_action_flag_valid.sh
@@ -28,6 +28,13 @@ function test_action_flag_valid_optional_without_value {
 	echo "$flag"
 }
 
+function test_action_flag_valid_joint_args_invalid_shortname {
+	__addarg "" "--flag" "flag" "optional" "" "help text"
+	__addarg "-g" "--glad" "flag" "optional" "" "help text"
+	__parseargs "$@"
+	echo "$flag $glad"
+}
+
 # ========= ASSERTIONS ========= #
 function koitest_run {
 	runtest test_action_flag_valid_required "1" "-f"
@@ -35,4 +42,5 @@ function koitest_run {
 	runtest test_action_flag_valid_no_consume_arguments "1 arg" "-f" "-a" "arg"
 	runtest test_action_flag_valid_optional_with_value "1" "-f"
 	runtest test_action_flag_valid_optional_without_value "0"
+	runtest test_action_flag_valid_joint_args_invalid_shortname __error__ "-fg"
 }
